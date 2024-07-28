1c3160a4d63a8c21766987af87acc87308630ea6
Talibiq123 <talibsaeed90@gmail.com>
2024-07-28 21:32:07 +0530

    useRef hook

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
+import { useRef } from 'react'
 
 const Todo = () => {
+
+  const inputRef = useRef()
+
+  const add = () => {
+    const inoutText = inputRef.current.value;
+    console.log(inoutText);
+  }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl '>
       {/* ----------title---------- */}
@@ -13,8 +22,8 @@ const Todo = () => {
 
       {/* input feild  */}
       <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input className='bg-transparent border-0 outline-none flex-1 h-12 pl-6 pr-2 placeholder: text-slate-600 ' type="text" placeholder="Add you task here." />
-        <button className='border-none rounded-full bg-gray-600 w-32 h-12 text-black text-lg font-medium cursor-pointer  '>Add</button>
+        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-12 pl-6 pr-2 placeholder: text-slate-600 ' type="text" placeholder="Add you task here." />
+        <button onClick={add} className='border-none rounded-full bg-gray-600 w-32 h-12 text-black text-lg font-medium cursor-pointer  '>Add</button>
       </div>
 
       <div>
